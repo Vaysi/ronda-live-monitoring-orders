5f2c05b41f0560d775c3b50aa8f6db4838256333
Abolfazl Vaysi <vaysi.erfan@gmail.com>
2020-12-30 13:21:08 +0330

    Add Search

--- src/components/orders-list.tsx
+++ src/components/orders-list.tsx
@@ -1,6 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { makeStyles, Theme } from "@material-ui/core/styles";
-import { DataGrid, ColDef, ValueGetterParams } from "@material-ui/data-grid";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
@@ -8,8 +7,14 @@ import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
-import { Button, ButtonGroup, Typography } from "@material-ui/core";
-import { Headset, Motorcycle, Store } from "@material-ui/icons";
+import {
+  Button,
+  ButtonGroup,
+  Input,
+  InputAdornment,
+  Typography,
+} from "@material-ui/core";
+import { Headset, Motorcycle, Search, Store } from "@material-ui/icons";
 
 const useStyles = makeStyles((theme: Theme) => ({
   root: {
@@ -20,11 +25,21 @@ const useStyles = makeStyles((theme: Theme) => ({
     minWidth: 650,
   },
   icon: {
-    marginLeft: theme.spacing(1)
+    marginLeft: theme.spacing(1),
   },
   date: {
-    direction: "ltr"
-  }
+    direction: "ltr",
+  },
+  margin: {
+    margin: theme.spacing(1),
+  },
+  search: {
+    paddingBottom: theme.spacing(1),
+    paddingTop: theme.spacing(1),
+  },
+  searchIcon: {
+    marginLeft: theme.spacing(1),
+  },
 }));
 
 interface Props {
@@ -35,8 +50,45 @@ interface Props {
 export default function OrdersList(props: Props) {
   const classes = useStyles();
 
+  const [term, setTerm] = useState<string>("");
+  const [searchList, setSearchList] = useState<any[]>([]);
+
+  useEffect(() => {
+    if (term == "") {
+      setSearchList(props.list);
+    }
+  }, [props.list]);
+
+  const searchTerm = (evt: any) => {
+    if (evt.target.value.replace(" ", "") == "") {
+      setTerm("");
+      setSearchList(props.list);
+    } else {
+      setTerm(evt.target.value);
+      setSearchList(
+        props.list.filter((item) =>
+          item.id.toString().includes(evt.target.value)
+        )
+      );
+    }
+  };
+
   return (
     <div className={classes.root}>
+      <div className={classes.margin}>
+        <Input
+          startAdornment={
+            <InputAdornment position="start">
+              <Search className={classes.searchIcon} />
+            </InputAdornment>
+          }
+          placeholder="جستجو"
+          fullWidth={true}
+          className={classes.search}
+          value={term}
+          onChange={searchTerm}
+        />
+      </div>
       <TableContainer component={Paper}>
         <Table className={classes.table}>
           <TableHead>
@@ -49,22 +101,26 @@ export default function OrdersList(props: Props) {
             </TableRow>
           </TableHead>
           <TableBody>
-            {props.list.length &&
-              props.list.map((row) => (
+            {searchList.length &&
+              searchList.map((row) => (
                 <TableRow key={row.id}>
                   <TableCell component="th" scope="row" align="center">
                     {row.id}
                   </TableCell>
                   <TableCell align="center">{row.statusText}</TableCell>
-                  <TableCell align="center" className={classes.date}>{row.storedAt}</TableCell>
-                  <TableCell align="center" className={classes.date}>{row.createdAt}</TableCell>
+                  <TableCell align="center" className={classes.date}>
+                    {row.storedAt}
+                  </TableCell>
+                  <TableCell align="center" className={classes.date}>
+                    {row.createdAt}
+                  </TableCell>
                   <TableCell align="center">
                     <ButtonGroup orientation="vertical" variant="outlined">
                       {row.supporter && (
                         <Button variant="contained" color="primary">
                           <Headset className={classes.icon} />
                           <Typography>
-                            پشتیبان  : ‌
+                            پشتیبان : ‌
                             {`${row.supporter.firstName} ${row.supporter.lastName}`}
                           </Typography>
                         </Button>
@@ -73,7 +129,7 @@ export default function OrdersList(props: Props) {
                         <Button variant="contained" color="secondary">
                           <Motorcycle className={classes.icon} />
                           <Typography>
-                            پیک  : ‌
+                            پیک : ‌
                             {`${row.biker.firstName} ${row.biker.lastName}`}
                           </Typography>
                         </Button>
@@ -82,7 +138,7 @@ export default function OrdersList(props: Props) {
                         <Button variant="contained" color="default">
                           <Store className={classes.icon} />
                           <Typography>
-                            سبدچین :   
+                            سبدچین :
                             {` ${row.picker.firstName} ${row.picker.lastName}`}
                           </Typography>
                         </Button>
